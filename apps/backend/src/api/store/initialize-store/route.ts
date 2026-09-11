b7957616c69002a72983dbb3010d606f3d2128d1
Cpidar <sayeyesepidar@gmail.com>
2026-09-11 19:22:48 +0330

    feat: update price cards with new cart store

--- apps/backend/src/api/store/initialize-store/route.ts
+++ apps/backend/src/api/store/initialize-store/route.ts
@@ -2,15 +2,19 @@ import type { MedusaRequest, MedusaResponse } from "@medusajs/framework/http";
 import { default_data_seed } from "../../../scripts/defaul-seed";
 import { seedProgress } from "../../../utils/initialize-store-progress";
 import { StoreDTO } from "@medusajs/types";
+import { Modules } from "@medusajs/framework/utils";
 
 export async function POST(req: MedusaRequest, res: MedusaResponse) {
+
+    const locking = req.scope.resolve(Modules.LOCKING)
+    const currentStore = req.scope.resolve("currentStore") as StoreDTO
+    const storeId = currentStore.id
+
     if (seedProgress.isRunning()) {
         return res.status(409).json({ message: "عملیات seed در حال حاضر در حال اجراست." });
     }
 
-    const currentStore = req.scope.resolve("currentStore") as StoreDTO;
 
-    const { id: storeId } = currentStore;
 
     seedProgress.start();
 
